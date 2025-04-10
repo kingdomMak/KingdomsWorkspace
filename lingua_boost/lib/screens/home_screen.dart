ff5db95d85c02ac80fcbd6c3a32b93ad22fe5b2d
Kingdom Makunamisha <your@email.com>
2025-04-10 15:10:54 +0200

    ⚙️ Add core app and Django REST framework setup

--- lingua_boost/lib/screens/home_screen.dart
+++ lingua_boost/lib/screens/home_screen.dart
@@ -1,32 +1,17 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('LinguaBoost'),
+        title: const Text('PepPal 💖'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Welcome to LinguaBoost 👋',
-              style: TextStyle(fontSize: 22),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // Navigate to quiz screen (coming next)
-              },
-              child: const Text('Start Learning'),
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text('Welcome to your positivity buddy!'),
       ),
     );
   }
-} 
+}
