1a4d7ad893d9989bbd354b898f5ee47d1250e0ce
Kingdom Makunamisha <your@email.com>
2025-04-10 15:53:54 +0200

    🔄 Add pull-to-refresh functionality to daily affirmation screen

--- lingua_boost/peppal_backend/core/views.py
+++ lingua_boost/peppal_backend/core/views.py
@@ -5,9 +5,9 @@ from .models import Affirmation
 from .serializers import AffirmationSerializer
 
 # Create your views here.
-
 class AffirmationListView(APIView):
     def get(self, request):
-        affirmations = Affirmation.objects.all().order_by('-created_at')[:1]
+        affirmations = Affirmation.objects.all().order_by('-created_at')  # No limit!
         serializer = AffirmationSerializer(affirmations, many=True)
         return Response(serializer.data)
+
